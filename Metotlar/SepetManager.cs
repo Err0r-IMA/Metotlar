f3abb810c692d61766ae9576bef6b72250ab496f
Murat ahmet Kıroğlu <mrt_drncli4161@outlook.com>
2021-01-21 18:10:40 +0300

    21.01.2021 son hali

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+
+    //naming convention
+    
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(" Tebrikler. sepete eklendi : " + urun.Adi);
+        
+        
+        
+        }
+
+        
+
+
+    }
+}
---
+++ Metotlar/Urun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Urun
+    {
+        public int ID { get; set; }
+       
+        public string Adi { get; set; }
+       
+        public double Fiyati { get; set; }
+        
+        public string Aciklama { get; set; }
+        
+        //prop int StokAdedi { get; set; }
+
+
+
+    }
+}
